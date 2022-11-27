667b8e917920a883e63aac966c73c009b2d1e145
suhas <suhasa05@gmail.com>
2022-11-27 13:50:46 +0530

    html change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LocationSelectorComponent } from './location-selector/location-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationSelectorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
